16d06190c02c0ae51913be9fb78c9583ffe81d72
automatizacionconia <automatizacionconia.agentes@gmail.com>
2025-04-21 16:20:10 -0500

    Delete .nixpacks/assets/prestart.mjs

--- .nixpacks/assets/prestart.mjs
+++
@@ -1,7 +0,0 @@
-import { $ } from "zx";
-
-await $`mkdir -p /var/log/nginx`;
-await $`touch /var/log/nginx/error.log`;
-await $`chmod 755 /var/log/nginx`;
-
-console.log("✔️ Nginx log file created (or already exists)");
